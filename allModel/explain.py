6d58b2f278fec6dd37f25a9af8bdd33929617f88
root <704382210@qq.com>
2020-03-31 18:03:42 +0800

    李世辉-更新代码库-添加说明Py和BUG处理

---
+++ allModel/explain.py
@@ -0,0 +1,3 @@
+# pip install django==2.1.7
+# pip install requests==2.22.0
+# python manage.py inspectdb > allModel/userModel.py  --创建数据库实体表
--- views/userViews.py
+++ views/userViews.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,render_to_response
-from  allModel import userModel
+from allModel import userModel
+
 # 访问首页
 def index(request):
     return render_to_response("user/index.html")
@@ -10,11 +11,11 @@ def register(request):
         username = request.POST.get("username",None)
         password = request.POST.get("password",None)
         usercode = request.POST.get("usercode",None)
-        twz = userModel.user.objects.create(username=username, password=password, usercode=usercode)
+        twz = userModel.AllmodelUser.objects.create(username=username, password=password, usercode=usercode)
         twz.save()
     return render(request,"user/register.html")
 
 # 展示用户信息
 def userList(request):
-    user_list = userModel.user.objects.all()
+    user_list = userModel.AllmodelUser.objects.all()
     return render(request,"user/showUser.html",{"userList":user_list})
