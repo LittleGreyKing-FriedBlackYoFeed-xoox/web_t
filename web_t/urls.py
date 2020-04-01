be8a937069e83bf18d8df671eceab451f9ab32b4
root <704382210@qq.com>
2020-04-01 17:23:58 +0800

    Merge branch 'master' of C:\Users\TCTM\Desktop\web_t with conflicts.

--- web_t/urls.py
+++ web_t/urls.py
@@ -1,15 +1,14 @@
 from django.contrib import admin
 from django.conf.urls import *
+
 from allModel.userdemo.userViews import index,register,userList
 from allModel.xiaohuihui.studentViews import student
 from django.conf import settings
 from django.conf.urls.static import static
-from allModel.jz.jzViews import jz
 urlpatterns = [
     url('admin/', admin.site.urls),
     url('^index/$',index),
     url('^register/$',register),
     url('^userList/$',userList),
-    url('^student/$',student),
-    url('^jz/$',jz),
+    url('^s/$',student),
 ]+ static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
