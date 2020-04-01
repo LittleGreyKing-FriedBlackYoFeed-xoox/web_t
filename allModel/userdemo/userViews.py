9a6fa4be13779755bcc6143deffaff97bcb95f86
TCTM <704382210@qq.com>
2020-04-01 17:20:30 +0800

    焦震.目录更改

--- allModel/userdemo/userViews.py
+++ allModel/userdemo/userViews.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render,render_to_response
-from allModel.userdemo import userModel
-
-
+from  allModel import userModel
 # 访问首页
 def index(request):
     return render_to_response("user/index.html")
@@ -12,11 +10,11 @@ def register(request):
         username = request.POST.get("username",None)
         password = request.POST.get("password",None)
         usercode = request.POST.get("usercode",None)
-        twz = userModel.AllmodelUser.objects.create(username=username, password=password, usercode=usercode)
+        twz = userModel.user.objects.create(username=username, password=password, usercode=usercode)
         twz.save()
-    return render(request, "user/register.html")
+    return render(request,"user/register.html")
 
 # 展示用户信息
 def userList(request):
-    user_list = userModel.AllmodelUser.objects.all()
-    return render(request, "user/showUser.html", {"userList":user_list})
+    user_list = userModel.user.objects.all()
+    return render(request,"user/showUser.html",{"userList":user_list})
--- web_t/urls.py
+++ web_t/urls.py
@@ -4,10 +4,12 @@ from allModel.userdemo.userViews import index,register,userList
 from allModel.xiaohuihui.studentViews import student
 from django.conf import settings
 from django.conf.urls.static import static
+from allModel.jz.jzViews import jz
 urlpatterns = [
     url('admin/', admin.site.urls),
     url('^index/$',index),
     url('^register/$',register),
     url('^userList/$',userList),
     url('^student/$',student),
+    url('^jz/$',jz),
 ]+ static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
