9170ec9478e0d626fb077e1190aee17587b851ea
TCTM <704382210@qq.com>
2020-04-01 18:57:12 +0800

    焦震.目录更改

--- web_t/urls.py
+++ web_t/urls.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
 from django.conf.urls import *
-
 from allModel.userdemo.userViews import index,register,userList
 from allModel.xiaohuihui.studentViews import student
 from django.conf import settings
 from django.conf.urls.static import static
+from allModel.jz.jzViews import jz
+from allModel.zhangshuting.zzViews import students
 urlpatterns = [
     url('admin/', admin.site.urls),
     url('^index/$',index),
     url('^register/$',register),
     url('^userList/$',userList),
-    url('^s/$',student),
+    url('^student/$',student),
+    url('^jz/$',jz),
+    url('^shuting/$',students),
 ]+ static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
