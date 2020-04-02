8a6a1e3aa55fe9b2e409a4fd038a02c8da2aaf02
灰灰老师 <50125083+LittleGreyKing-FriedBlackYoFeed-xoox@users.noreply.github.com>
2020-04-02 14:27:09 +0800

    Update urls.py

--- web_t/urls.py
+++ web_t/urls.py
@@ -7,21 +7,14 @@ from django.conf.urls.static import static
 <<<<<<< Updated upstream
 from allModel.jz.jzViews import jz
 from allModel.zhangshuting.zzViews import students
-=======
-
 from allModel.zhaojiawei.zzView import rename
-
->>>>>>> Stashed changes
 urlpatterns = [
     url('admin/', admin.site.urls),
     url('^index/$',index),
     url('^register/$',register),
     url('^userList/$',userList),
     url('^student/$',student),
-<<<<<<< Updated upstream
     url('^jz/$',jz),
     url('^shuting/$',students),
-=======
     url('^xxxx/$',rename),
->>>>>>> Stashed changes
 ]+ static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
