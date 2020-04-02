2da1d4d61d0e36f4b2cc9c7ee06e96a6431ced63
赵嘉伟 <349144944@qq.com>
2020-04-02 13:55:39 +0800

    今天的赵老师

--- web_t/urls.py
+++ web_t/urls.py
@@ -4,14 +4,24 @@ from allModel.userdemo.userViews import index,register,userList
 from allModel.xiaohuihui.studentViews import student
 from django.conf import settings
 from django.conf.urls.static import static
+<<<<<<< Updated upstream
 from allModel.jz.jzViews import jz
 from allModel.zhangshuting.zzViews import students
+=======
+
+from allModel.zhaojiawei.zzView import rename
+
+>>>>>>> Stashed changes
 urlpatterns = [
     url('admin/', admin.site.urls),
     url('^index/$',index),
     url('^register/$',register),
     url('^userList/$',userList),
     url('^student/$',student),
+<<<<<<< Updated upstream
     url('^jz/$',jz),
     url('^shuting/$',students),
+=======
+    url('^xxxx/$',rename),
+>>>>>>> Stashed changes
 ]+ static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
